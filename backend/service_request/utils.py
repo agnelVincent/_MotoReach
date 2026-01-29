e7ead9196bcf86cb7520feb55149c665143ee9c1
agnel <agnelvincent779@gmail.com>
2026-01-29 19:10:50 +0530

    done automatic status updating of mechanic after service completion

--- backend/service_request/utils.py
+++ backend/service_request/utils.py
@@ -10,12 +10,23 @@ def check_request_expiration(service_request):
         return False
 
     if service_request.expires_at < timezone.now():
+        from .models import ServiceExecution
 
-        
         is_refunded, msg = check_and_process_refund(service_request)
         
         service_request.status = 'EXPIRED'
         service_request.save()
+
+        try:
+            execution = service_request.execution
+            if execution:
+                for mechanic in execution.mechanics.all():
+                    mechanic.availability = 'AVAILABLE'
+                    mechanic.save()
+                execution.delete()
+        except ServiceExecution.DoesNotExist:
+            pass
+
         return True
     
     return False
