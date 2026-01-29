510b4219a05e3d1df30cb8ab9b139a875498d068
agnel <agnelvincent779@gmail.com>
2026-01-29 19:32:00 +0530

    added acrolltotop.jx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,13 @@ import { Toaster } from 'react-hot-toast';
 
 
 
+import ScrollToTop from './components/ScrollToTop';
+
 function App() {
   return (
 
     <BrowserRouter>
+      <ScrollToTop />
       <Toaster position="top-right" />
       <AppRoute />
     </BrowserRouter>
