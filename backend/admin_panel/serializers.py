8903867d523e27b77ff0d48bfaaa7e28f72494ac
agnel <agnelvincent779@gmail.com>
2026-04-14 16:31:21 +0530

    changed field name in complaint serializer

--- backend/admin_panel/serializers.py
+++ backend/admin_panel/serializers.py
@@ -3,7 +3,7 @@ from .models import Complaint
 
 class ComplaintSerializer(serializers.ModelSerializer):
     reporter = serializers.CharField(source = 'reporter.full_name')
-    reported_by = serializers.CharField(source = 'reported_user.full_name')
+    reported_user = serializers.CharField(source = 'reported_user.full_name')
     class Meta:
         model = Complaint
         fields = '__all__'
