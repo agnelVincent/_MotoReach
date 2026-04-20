1ca278b172eeed50a03d453649b2fa16a0bc10ac
agnel <agnelvincent779@gmail.com>
2026-04-21 02:08:47 +0530

    added useeffect in app.jsx to kickout expired cookie users staying in static pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getProfile } from './redux/slices/ProfileSlice';
 import './index.css';
 import AppRoute from './routes/AppRoute';
 import { Toaster } from 'react-hot-toast';
@@ -8,6 +11,15 @@ import { Toaster } from 'react-hot-toast';
 import ScrollToTop from './components/ScrollToTop';
 
 function App() {
+  const dispatch = useDispatch();
+  const { isAuthenticated } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      dispatch(getProfile());
+    }
+  }, [dispatch, isAuthenticated]);
+
   return (
 
     <BrowserRouter>
